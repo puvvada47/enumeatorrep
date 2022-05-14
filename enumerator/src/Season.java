fa6b9703857b79dc92aa6cd6d5f6288ff70555f0
puvvada47 <puvvada1210609147@gmail.com>
2022-05-14 17:04:16 +0530

    enumerator is required
    commit

---
+++ enumerator/src/Season.java
@@ -0,0 +1,49 @@
+public enum Season {
+
+	WINTER(1, "COLD"), 
+	SUMMER(2, "HOT"), 
+	MONSOON(3, "CHILLY");
+
+	private int id;
+	private String taste;
+
+	private Season(int id, String taste) {
+		this.id = id;
+		this.taste = taste;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public String getTaste() {
+		return taste;
+	}
+
+	public static void main(String[] args) {
+
+		Season s = Season.WINTER;
+		System.out.println(s);
+		System.out.println(s.name());
+		System.out.println(s.getId()+":"+s.getTaste()+":"+s.ordinal()+":"+s.toString()+":"+s.name()+"::"+s.id+":"+s.taste);
+		
+		for (Season a : Season.values()) {
+			System.out.println(a.ordinal());
+			System.out.println(a + "::" + a.toString() + "::" + a.name() + "::"
+					+ a.getId() + "::" + a.getTaste());
+			
+			System.out.println(a);
+			System.out.println(a.ordinal());
+			System.out.println(a.name());
+			System.out.println(a.toString());
+			System.out.println(a.getId());
+			System.out.println(a.getTaste());
+			System.out.println(a.id);
+			System.out.println(a.taste);
+		}
+		
+		
+		
+	}
+
+}
